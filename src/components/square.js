70bfbe52f724c42ba07adec35363fdc8c5a93231
William <wphokomp@student.wethinkcode.co.za>
2019-08-22 15:43:23 +0200

    Board skeleton

---
+++ src/components/square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className='square'>
+                {}
+            </button>
+        )
+    }
+}
+
+export default Square;
